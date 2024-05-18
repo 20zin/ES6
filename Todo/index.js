d35d9429dc536894bddf064f0e25e993f6e44fa2
YoungjinLee <117518123+20zin@users.noreply.github.com>
2024-05-18 18:46:35 +0900

    todos

--- Todo/index.js
+++ Todo/index.js
@@ -2,15 +2,64 @@ const form = document.querySelector('form');
 const input = document.querySelector('input');
 const ul = document.querySelector('ul')
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault(); //값 저장
-    
-    if(input.value !== ''){
+
+let todos = [];
+
+const save = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+const delItem = (event) => {
+    const target = event.target.parentElement;
+
+    todos.filter((todo) => todo.id !== parseInt(target.id));
+    save();
+
+    target.remove();
+}
+
+const addItem = (todo) => {
+    if(todo.text !== ''){
         const li = document.createElement('li');
-        li.innerText = input.value;
-        ul.appendChild(li); //차례대로 저장
+        const button = document.createElement('button');
+        const span = document.createElement('span');
 
-        input.value = '';
+        span.innerText = todo.text;
+        button.innerText = '삭제';
+        button.addEventListener('click', delItem);
+
+        li.appendChild(span);
+        li.appendChild(button);
+        ul.appendChild(li);
+        li.id = todo.id;
     }
-});
+}
 
+const handler = (event) => {
+    event.preventDefault();
+
+    const todo = {
+        id:Date.now(),
+        text:input.value,
+    };
+
+    todos.push(todo);
+    addItem(todo);
+    save();
+
+    input.value = '';
+}
+
+const init = () => {
+    const userTodos = JSON.stringify(localStorage.getItem('todos'));
+
+    if(userTodos){
+    userTodos.forEach((todo) => {
+        addItem(todo);
+    });
+    
+    todos = userTodos;
+    }
+}
+
+form.addEventListener('submit', handler);
